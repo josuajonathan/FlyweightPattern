4a822f69b49a8d209e5d9f32cc170b5b08ed1bb6
Josua Jonathan Manurung <87791029+josuajonathan@users.noreply.github.com>
2022-04-11 01:46:15 +0700

    Create Objek.h

---
+++ Objek.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Objek {
+public :
+	Objek();
+	virtual void settipe() = 0;
+	virtual string gettipe() = 0;
+
+	virtual void setukuran() = 0;
+	virtual double getukuran() = 0;
+
+	virtual void setKoord() = 0;
+	virtual int getKoordX() = 0;
+	virtual int getKoordY() = 0;
+
+	virtual void enableObjek() = 0;
+	virtual void disableObjek() = 0;
+	virtual bool getStatus() = 0;
+
+	virtual void draw();
+};
