30dffedb44bbf8b77d260adb65bcea397466ef39
Josua Jonathan Manurung <87791029+josuajonathan@users.noreply.github.com>
2022-04-11 01:47:51 +0700

    Update Objek.h

--- Objek.h
+++ Objek.h
@@ -6,19 +6,19 @@ using namespace std;
 class Objek {
 public :
 	Objek();
-	virtual void settipe() = 0;
+	virtual void settipe()   = 0;
 	virtual string gettipe() = 0;
 
-	virtual void setukuran() = 0;
+	virtual void setukuran()   = 0;
 	virtual double getukuran() = 0;
 
 	virtual void setKoord() = 0;
 	virtual int getKoordX() = 0;
 	virtual int getKoordY() = 0;
 
-	virtual void enableObjek() = 0;
-	virtual void disableObjek() = 0;
-	virtual bool getStatus() = 0;
+	virtual void enableObj()  = 0;
+	virtual void disableObj() = 0;
+	virtual bool getStatus()  = 0;
 
 	virtual void draw();
 };
